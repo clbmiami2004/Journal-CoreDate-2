bcecafa07cd9941880071b9ad5180ffc3ec40f1f
Christian Lorenzo <clbmiami2004@gmail.com>
2019-12-06 20:51:45 -0800

    Initial Commit

---
+++ Journal-CoreDate-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal-CoreDate-2
+//
+//  Created by Lambda_School_Loaner_201 on 12/6/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
